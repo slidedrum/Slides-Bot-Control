f3ec69223bd4d26b4b92308d953b679ece6b5f9f
SlideDrum <slidedrumofficial@gmail.com>
2026-06-17 21:28:07 -0400

    Fixed cancel all erroring before you send an action.

--- SmartSelect/PressActions/DoubleTapActions/pActionCancelAll.cs
+++ SmartSelect/PressActions/DoubleTapActions/pActionCancelAll.cs
@@ -36,6 +36,7 @@ namespace BotControl.SmartSelect.PressActions.TapActions
             // Candidate is irrelevant for this action, we just need to check if we have any bots selected
             bool facingUp = Vector3.Angle(zStaticRefrences.CameraTransform.forward, Vector3.up) < 15f;
             if (!facingUp) return false;
+            if (!zActions.manualActions.ContainsKey(zStaticRefrences.LocalPlayer.CharacterID)) return false;
             if (zActions.manualActions[zStaticRefrences.LocalPlayer.CharacterID].Count <= 1) return false;
             return true;
         }
